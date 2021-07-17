0937874187ccbc757057317847715665f20b30c8
David Jeannette <31715021+dajea24@users.noreply.github.com>
2021-07-17 15:24:39 -0400

    Delete RCNN.py

--- RCNN.py
+++
@@ -1,7 +0,0 @@
-# Architecture and weight files for the model
-import cv2
-
-textGraph = "./mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"
-modelWeights = "./frozen_inference_graph.pb"
-# Load the network
-net = cv2.dnn.readNetFromTensorflow(modelWeights, textGraph)
